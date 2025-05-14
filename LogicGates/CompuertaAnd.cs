549b471ba1c88b79f08cfc91e5273726e7dc86ec
ramaffxt <ramiro.perez@correo.ucu.edu.uy>
2025-05-14 19:18:01 -0300

    Creacion de la clase CompuertaAnd, junto con ValorVerdadero

---
+++ LogicGates/CompuertaAnd.cs
@@ -0,0 +1,37 @@
+﻿namespace DefaultNamespace;
+
+public class CompuertaAnd: ValorVerdadero
+{
+    private string name;
+    private List<IValorVerdad> entradas = new List<IValorVerdad>();
+    public CompuertaAND(string nombre, IValorVerdad entrada)
+    {
+        this.nombre = nombre;
+        this.entradas.Add(entrada);
+    }
+    public string GetNombre()
+    {
+        return this.nombre;
+    }
+    public List<IValorVerdad> GetEntradas()
+    {
+        return this.entradas;
+    }
+    public void AgregarEntrada(IValorVerdad entrada)
+    {
+        this.entradas.Add(entrada);
+    }
+    public bool Evaluar
+    {
+        get
+        {
+            bool resultado = this.entradas[0].Evaluar;
+            for (int i = 1; i < this.entradas.Count; i++)
+            {
+                resultado = resultado && this.entradas[i].Evaluar;
+            }
+
+            return resultado;
+        }
+    }
+}
